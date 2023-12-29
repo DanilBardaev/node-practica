edb5135938dd088661492ad87533e8122e116417
Danil Bardaev <35bardaev@gmail.com>
2023-12-29 16:23:14 +0300

    4 6

--- controllers/login.js
+++ controllers/login.js
@@ -1,20 +1,40 @@
 const User = require("../models/user");
+const validator = require("validator");
 
 exports.form = (req, res) => {
   res.render("loginForm", { title: "Login" });
 };
 
 exports.submit = (req, res, next) => {
+  const email = req.body.loginForm.email;
+  const password = req.body.loginForm.password;
+  if (!validator.isEmail(email)) {
+    const errors = {
+      email: {
+        msg2: '*Неверный формат адреса электронной почты. example@gmail.com'
+      }
+    };
+    return res.render("loginForm", { errors });
+  }
+  if (password.length < 8) {
+    const errors = {
+      password: {
+        msg: '*Пароль должен содержать не менее 8 символов. Или он неверный.'
+      }
+    };
+    return res.render("loginForm", { errors });
+  }
   User.authenticate(req.body.loginForm, (error, data) => {
     if (error) return next(error);
     if (!data) {
-      console.log("Имя или пароль неверный");
-      res.redirect("back");
-    } else {
-      req.session.userEmail = data.email;
-      req.session.userName = data.name;
-      res.redirect("/");
+      const error = new Error("Имя или пароль неверный");
+      error.statusCode = 401; 
+      return next(error);
     }
+    
+    req.session.userEmail = data.email;
+    req.session.userName = data.name;
+    res.redirect("/");
   });
 };
 
--- controllers/register.js
+++ controllers/register.js
@@ -1,28 +1,42 @@
 const User = require("../models/user");
+const validator = require("validator");
 
 exports.form = (req, res) => {
   res.render("registerForm", { errors: {} });
 };
 
 exports.submit = (req, res, next) => {
-  if (req.body.password.length < 8) {
+  const { email, password } = req.body;
+
+
+  if (password.length < 8) {
     const errors = {
       password: {
-        msg: '*Пароль должен содержать не менее 8 символов.'
+        msg: 'Пароль должен содержать не менее 8 символов.'
       }
     };
     return res.render("registerForm", { errors });
   }
 
-  User.findByEmail(req.body.email, (error, user) => {
+  // Проверка валидности введенного адреса электронной почты
+  if (!validator.isEmail(email)) {
+    const errors = {
+      email: {
+        msg2: '*Неверный формат адреса электронной почты. example@gmail.com'
+      }
+    };
+    return res.render("registerForm", { errors });
+  }
+
+  User.findByEmail(email, (error, user) => {
     if (error) return next(error);
     if (user) {
-      console.log("Такой пользователь в базе уже существует");
+      console.log("Такой пользователь в базе уже существует.");
       res.redirect("/");
     } else {
       User.create(req.body, (err) => {
         if (err) return next(err);
-        req.session.userEmail = req.body.email;
+        req.session.userEmail = email;
         req.session.userName = req.body.name;
         res.redirect("/");
       });
